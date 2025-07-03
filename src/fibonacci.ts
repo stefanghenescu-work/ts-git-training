89960fd40f528edb82206af302869ea737127d2f
Stefan Ghenescu <Stefan.Ghenescu@softwire.com>
2025-07-03 17:00:25 +0300

    Resolve merge conflict for recursive option

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,4 +1,8 @@
-export const computeFibonacciNumber = (position: number | null): number => {
+export const computeFibonacciNumber = (position: number | null, recursion: boolean = false): number => {
+    if (recursion) {
+        return recursiveFibonacci(1, 1, position - 2);
+    }
+    
     let notNullPosition = position;
     if (notNullPosition === null) {
         notNullPosition = 1;   
@@ -24,7 +28,6 @@ export const computeFibonacciNumber = (position: number | null): number => {
     }
     return j;
 };
-
 const computeNegativeFibonacci = (position: number): number => {
     if (position >= 0) {
         throw new Error(`Position must be less than zero! Received: ${position}.`);
@@ -37,4 +40,16 @@ const computeNegativeFibonacci = (position: number): number => {
 export const computeFibonacciArray = (start: number, endInclusive: number): number[] => {
     const inputArray = [...Array(endInclusive - start + 1).keys()].map(i => i + start);
     return inputArray.map(x => computeFibonacciNumber(x));
+}
+
+const recursiveFibonacci = (previous: number, current: number, stepsLeft: number): number => {
+    if (stepsLeft < 0) {
+        return 1;
+    }
+    switch (stepsLeft) {
+        case 0:
+            return current;
+        default:
+            return recursiveFibonacci(current, previous + current, stepsLeft - 1);
+    }
 }
