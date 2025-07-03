fffeff48c643c3555ea52cf6bc69bd278b92d072
Stefan Ghenescu <Stefan.Ghenescu@softwire.com>
2025-07-03 17:32:50 +0300

    Resolve merge conflic for variable naming

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -14,17 +14,17 @@ export const computeFibonacciNumber = (position: number | null, recursion: boole
         return 1;
     }
 
-    let i = 1;
-    let j = 1;
+    let smallFibonacciNumber = 1;
+    let largeFibonacciNumber = 1;
 
     let currentPosition = 2;
-    while (currentPosition < notNullPosition) {
-        const temp = i;
-        i = j;
-        j += temp;
+    while (currentPosition < position) {
+        const nextFibonacciNumber = smallFibonacciNumber + largeFibonacciNumber;
+        smallFibonacciNumber = largeFibonacciNumber;
+        largeFibonacciNumber = nextFibonacciNumber;
         currentPosition++;
     }
-    return j;
+    return largeFibonacciNumber;
 };
 
 const computeNegativeFibonacci = (position: number): number => {
